4a6b2c97cb5d4864305ae90df2a05f685db2c978
Martin Jordanov <xxm0703@abv.bg>
2017-09-11 16:28:52 +0300

    PHP Session

--- interested.php
+++ interested.php
@@ -1,4 +1,19 @@
-[
-		"catan",
-		"monopoly"
-]
+<?php
+
+session_start();
+var $game_names = [];
+$conn = mysqli_connect('localhost', 'root', '', 'users');
+if (!$conn) {
+    http_response_code(400);
+    exit();
+} else {
+    $ID = $_SESSION['ID'];
+    $log = mysqli_query($conn, "SELECT gameID FROM interes_games WHERE userID='$ID'");
+    while ($gameid = mysqli_fetch_array($log)) {
+        $game = mysqli_query($conn, "SELECT game_name FROM games WHERE ID='$gameid'");
+        while ($gamename = mysqli_fetch_array($game)){
+            
+        }
+    }
+}
+?>
