c7e6140abdbbe967ba170b02dd5502d82bd0c260
Martin Jordanov <xxm0703@abv.bg>
2017-09-11 15:09:01 +0300

    SQL

--- login.php
+++ login.php
@@ -13,14 +13,12 @@ if (!$conn){
 }else{
     $log = mysqli_query($conn,'SELECT password FROM users WHERE username == $_GET["username"]');
     if (!$log){
-        echo '<h1>bcdhsdbc</h1>';
-        header('http://localhost/events_people.html', true, 400);
-        mysqli_close($conn);
-        die(http_response_code(400));
+        header("Location: Profile.php#"); /* Redirect browser */
+        exit();
     }else {
         while ($var_dump = mysqli_fetch_array($log)) {
             if ($var_dump['password'] == $_POST['password']) {
-                header('Location: http://localhost/events_people.html',
+                header('Location: Profile.php#',
                     true,
                     202);
                 mysqli_close($conn);
