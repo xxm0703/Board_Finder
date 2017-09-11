f5d249360ca9d9c7a002e812c844fce03f88f370
Martin Jordanov <xxm0703@abv.bg>
2017-09-11 14:45:56 +0300

    Bug fix

--- login.php
+++ login.php
@@ -5,26 +5,20 @@
  * Date: 9/11/2017
  * Time: 11:42 AM
  */
-function Redirect($url, $permanent = false)
-{
-    if (headers_sent() === false)
-    {
-        header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
-    }
-
-    exit();
-}
 
 $conn = mysqli_connect('127.0.0.1','root', '','finder');
 if (!$conn){
-    http_response_code(404);
-    die(mysqli_connect_error());
+    http_response_code(503);
+    die("asd");
 }else{
     $log = mysqli_query($conn,'SELECT password FROM users WHERE username == $_GET["username"]');
     while ($var_dump = mysqli_fetch_array($log)){
-        if ($var_dump['password'] == $_GET['password']){
-            http_response_code(200);
-            Redirect('localhost:63342/BoardFinder/Events%20&%20People.html');
+        if ($var_dump['password'] == $_POST['password']){
+            header('Location: http://localhost/events_people.html',
+                true,
+                301);
+            mysqli_close($conn);
         }
     }
+    http_response_code(202);
 }
