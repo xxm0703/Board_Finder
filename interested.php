6aec23b48a6b5d3a420ab60d7ded25097eb5caf4
Martin Jordanov <xxm0703@abv.bg>
2017-09-11 16:56:51 +0300

    PHP Session

--- interested.php
+++ interested.php
@@ -1,19 +1,29 @@
 <?php
-
+/*
+ *
+ *
+ * */
 session_start();
-var $game_names = [];
-$conn = mysqli_connect('localhost', 'root', '', 'users');
+$game_names = array();
+$conn = mysqli_connect('localhost', 'root', '', 'finder');
 if (!$conn) {
     http_response_code(400);
     exit();
 } else {
-    $ID = $_SESSION['ID'];
-    $log = mysqli_query($conn, "SELECT gameID FROM interes_games WHERE userID='$ID'");
-    while ($gameid = mysqli_fetch_array($log)) {
-        $game = mysqli_query($conn, "SELECT game_name FROM games WHERE ID='$gameid'");
-        while ($gamename = mysqli_fetch_array($game)){
-            
+    if (!isset($_SESSION['ID'])){
+        header("Location: Profile.php#",true,400);
+        exit();
+    }else{
+        $ID = $_SESSION['ID'];
+        $log = mysqli_query($conn, "SELECT gameID FROM interes_games WHERE userID='$ID'");
+        while ($gameid = mysqli_fetch_array($log)) {
+            $game = mysqli_query($conn, "SELECT game_name FROM games WHERE ID='$gameid'");
+            while ($gamename = mysqli_fetch_array($game)) {
+                array_push($game_names, $gamename);
+            }
         }
     }
+//    echo $game_names;
+    exit();
 }
 ?>
